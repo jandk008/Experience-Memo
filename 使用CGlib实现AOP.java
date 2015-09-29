ee0ce0a896dc09a7f8c80cfef7d090655d0af156
Zhou, Zicheng <zczhou10@gmail.com>
2015-09-29 10:39:51 +0800

    Update 使用CGlib实现AOP.java

--- 使用CGlib实现AOP.java
+++ 使用CGlib实现AOP.java
@@ -10,7 +10,7 @@ enhancer.setCallback(proxy);
 
 
 //在cglib中，可以定义自己的代理类，只要实现 MethodInterceptor 这个接口，并将其实例设置在enhancer的setCallback方法中即可
-//这里使用了回调的概念，回调是闭包的一种体现。真正的实体函数会作为参数传入intercept方法，并在该方法中实现拦截，处理只后，再调//用真正的实体方法。
+//这里使用了回调的概念，回调是闭包的一种体现。真正的实体函数会作为参数传入intercept方法，并在该方法中实现拦截，处理只后，再调用真正的实体方法。
 public class ConnectionExceptionHandleProxy implements MethodInterceptor{
 	
 	private static Logger logger = Logger.getInstance(ConnectionExceptionHandleProxy.class);
@@ -18,6 +18,7 @@ public class ConnectionExceptionHandleProxy implements MethodInterceptor{
 	@Override
 	//arg0 是调用函数的对象，在这里即为ServiceExecutor的object对象
 	//arg1 是实体函数的method对象，每一次对函数的调用，都会被捕获到这里
+	//arg2 
 	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable  {
 		
 		CBTLogUtil.log(logger,LogLevel.INFO, "Start to execute "+arg1.getName()+ "....");
